fdb8cf9efca147503c0b540785b6de28e8f5dcca
edanurktly <kutluayedanur@gmail.com>
2022-10-06 19:46:30 +0300

    readdataa2

---
+++ ReadDataLab/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Reflection.Metadata;
+
+namespace ReadDataLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region soru1
+            //Ekrandan girilen iki sayinin birbiriyle toplaminin,
+            // birbirine olan farkinin birbirine olan kalani nedir 
+            #endregion
+
+            #region soru2
+            //dışarıdan girilen iki sayinini 10 eksiğinin karesinin 3e bolümünden kalan nedir?
+            #endregion
+
+            #region soru3
+            //dışarıdan girilen iki sayinin karelerinin toplami ile
+            //karelerinin farkinin toplami nedir?
+            #endregion
+
+            #region soru4
+            //vize notu %30 final notu 570 olan bir sistemde
+            //iki vize bir final notlarını sonucunda  geçme kalma durumunu yaziniz
+            #endregion
+
+            #region cevaplar
+
+            
+            //soru1
+
+            string? giris;
+            int sayi1, sayi2;
+            Console.WriteLine("Birinci sayiyi giriniz:");
+            giris = Console.ReadLine();
+            sayi1 = Convert.ToInt32(giris);
+
+            Console.WriteLine("İkinci sayiyi giriniz:");
+            giris = Console.ReadLine();
+            sayi2 = Convert.ToInt32(giris);
+            Console.WriteLine($"{sayi1} + {sayi2} 'nin toplamı {sayi1 + sayi2}");
+            Console.WriteLine($"{sayi1} - {sayi2} 'nin birbirine olan farki {sayi1 - sayi2}");
+            Console.WriteLine($"{sayi1} / {sayi2} 'nin kalani {sayi1 / sayi2}");
+
+
+            //soru2
+            string? ikincisoru;
+            int s1, s2;
+            Console.WriteLine("Birinci sayiyi giriniz:");
+            ikincisoru = Console.ReadLine();
+            s1 = Convert.ToInt32(ikincisoru);
+            Console.WriteLine($"{s1} 'in 10 eksiği {s1 - 10}dur");
+            Console.WriteLine($"{s1 - 10}'nin karesi  {(s1 - 10) * (s1 - 10)} 'dir.");
+            Console.WriteLine($" {(s1 - 10) * (s1 - 10)}'nin 3e bölümü {((s1 - 10) * (s1 - 10) / 3)}");
+
+            Console.WriteLine("İkinci sayiyi giriniz:");
+            ikincisoru = Console.ReadLine();
+            s2 = Convert.ToInt32(ikincisoru);
+            Console.WriteLine($"{s2} 'in 10 eksiği {s2 - 10}dur");
+            Console.WriteLine($"{s2 - 10}'nin karesi  {(s2 - 10) * (s2 - 10)} 'dir.");
+            Console.WriteLine($" {(s2 - 10) * (s2 - 10)}'nin 3e bölümü {((s2 - 10) * (s2 - 10) / 3)}");
+
+
+            //soru3
+            string? ücüncüsoru;
+            int a, b, kare, kare1, toplam;
+            Console.WriteLine("birinci sayiyi giriniz:");
+            ücüncüsoru = Console.ReadLine();
+            a = Convert.ToInt32(ücüncüsoru);
+            Console.WriteLine("ikinci sayiyi giriniz:");
+            ücüncüsoru = Console.ReadLine();
+            b = Convert.ToInt32(ücüncüsoru);
+
+
+
+            //soru4
+            float vize1, vize2, final, ortalama, sonuc;
+            //float bir degiskene manuel deger atilirsa sayinin sonuna f isareti konur
+
+            Console.WriteLine("birinci vizeyi giriniz:");
+            vize1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("ikinci vizeyi giriniz:");
+            vize2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("final notunuzu giriniz:");
+            final= Convert.ToSingle(Console.ReadLine());
+            ortalama = (vize1 + vize2) / 2;
+            sonuc =( ortalama * 0.3f) + (final * 0.7f);
+            Console.WriteLine($"sonuc:{sonuc}");
+            #endregion
+
+            float sayi1 = 12.5f;
+            decimal sayi2 = 12.5m;
+             
+            var a=
+        }
+
+    }
+    
+}
+ 
---
+++ ReadDataaa2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Reflection.Metadata;
+
+namespace ReadDataaa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Ekrandan Veri Okuma
+
+
+
+            ////Ekrandan Veri Okuma
+
+            ////Degisken tanimalamalari yapilirken veri tipinin sonuna 
+            ////Egerki ? isareti konulursa null deger alabilir demektir.
+            //string? giris;
+            //int sayi1, sayi2;
+            //DateTime trh;
+
+            //Console.WriteLine("Birinci sayiyi giriniz:");
+            //giris = Console.ReadLine(); //Ekrandan sayi okutuyoruz
+            //sayi1 = Convert.ToInt32(giris);
+
+            //Console.WriteLine("İkinci sayiyi giriniz:");
+            //giris = Console.ReadLine(); //Ekrandan sayi okutuyoruz
+            //sayi2 = Convert.ToInt32(giris);
+
+
+            //Console.WriteLine("tarih giriniz:");
+            //giris = Console.ReadLine();
+            // trh = Convert.ToDateTime(giris);
+
+
+
+
+
+            ////Ekranda göstermek için 1.yol
+            //Console.WriteLine("Girilen sayilar {0} VE {1}", sayi1, sayi2);
+            ////ekranda göstermke için 2. yol
+            //Console.WriteLine($"girilen sayilar {sayi1} ve {sayi2}");
+            ////ekranda göstermek için 3.yol
+            //Console.WriteLine("girilen sayilar" + sayi1 + " ve " + sayi2);
+
+            //Console.WriteLine("girilen tarih:" +trh);
+            #endregion
+
+
+            #region AritmetikIslemler
+            /*
+               Aritmetik Operatorler
+
+                 dört islem için: +,-,*,/
+                 kalan operatoru: %
+                  arttirma operatoru:++
+                  eksiltme operatoru:--
+            */
+            int a = 5; int b = 32; int c = 0; // int d = 5;
+
+            Console.WriteLine($"{b} nin {a} bolumunden kalan {b % a}");
+            c = a + b; //sonuc:37
+            c = 0;
+            b++;
+            c = a + b; //sonuc: 38
+            c = a + b++;
+            Console.WriteLine($"a'nin degeri:{a} b'nin degeri{b} sonuc:{c}");
+            c = a + b++;
+            Console.WriteLine($"a'nin degeri:{a} b'nin degeri{b} sonuc:{c}");
+            Console.WriteLine("------------------------");
+            c = a + ++b;
+            Console.WriteLine($"a'nin degeri:{a} b'nin degeri{b} sonuc:{c}");
+
+            c = ++a + ++b;
+            Console.WriteLine($"a'nin degeri:{a} b'nin degeri{b} sonuc:{c}");
+            // acik deklerasyon 
+            a++; //a=a+1
+            b++; //b=b+1
+            c = a + b; //
+
+            //Diger arttirma islemi
+            a += 1;
+            b -= 1;// b=b-1
+            a *= 2; //a=a*2;
+            Console.WriteLine("A=" + a);
+            a *= a;
+            Console.WriteLine("A=" + a);
+            a *= (a*=a);
+            Console.WriteLine("A=" + a);
+
+
+            #endregion
+
+            Console.WriteLine("cikmak icin enter tusuna basininiz...");
+            Console.ReadLine();
+        }
+
+    }
+}
