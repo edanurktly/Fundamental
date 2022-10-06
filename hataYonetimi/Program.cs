73611c71c64337d003f6e34094cfd3e98f8aa8de
edanurktly <kutluayedanur@gmail.com>
2022-10-06 20:01:23 +0300

    hatayönetimi

--- hataYonetimi/Program.cs
+++ hataYonetimi/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+
+
+            /*
+             * İSTİSNAİ TİPLER
+             * 1-ÇALIŞMA ZAMANI HATALARI: çoğunlukla kullanıcı kaynaklı hatalarıdır.
+             * 2-DERLEME ZAMANI HATALARI: yazılımcının yaptığı hatalardır.
+             * 3-MANTIKSAL HATALAR: en zor tespit edilen hata tipidir.
+             */
+
+            //breakpoint:programın çalışırken durmasını istediğimiz yer
+            //olursa kullandiğimşiz yöntemdir
+
+
+            //hata yönetimi try catch mekanizması kullanılır
+            // try==> deneme
+            //catch =>yakalamak 
+            int a = 5, b= 0, sonuc = 0;
+            try
+            {
+                sonuc = a / b;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // console.writeLine("sonuc:"+sonuc);
+;        }
     }
 }
