11a3ab2e6bbe0f8b7ebb84922a6e22d4d7516ec2
edanurktly <kutluayedanur@gmail.com>
2022-10-11 15:36:40 +0300

    donguler....

---
+++ Donguler/Program.cs
@@ -0,0 +1,242 @@
+﻿namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+          While Dongusu kosul dogru oldugu surece calişir
+         Eger koşul yanliş ise donguden cikacaktir.
+
+          */
+
+            // Asagidaki while dongusu çalişmaz
+            while (false)
+            {
+                Console.WriteLine("Helleo World");
+            }
+
+            #region Tek sayi girilence sonlanan döngü
+
+            //int sayi = 0;
+            //while (sayi % 2 == 0)
+            //{
+            //    Console.WriteLine("Sayi Giriniz");
+            //    sayi = int.Parse(Console.ReadLine());
+
+            //} 
+            #endregion
+
+            #region Sayinin basamaklarini bulan dongu
+            //Console.WriteLine("En az iki basamkli bir sayi giriniz:");
+            //int girilen = int.Parse(Console.ReadLine());
+            //int basamak = 0;
+
+            //while (girilen>0) 
+            //{ 
+            //    basamak++;
+            //    girilen = girilen / 10;
+            //}
+            //Console.WriteLine($"Girdiginiz sayi {basamak} basamaklidir");
+            #endregion
+
+
+            #region Ekrandan girilen sayi 0 olana kadar girilen sayilarin cift ve tek toplamlarini bulunuz
+
+            //int tektoplam=0, cifttoplam = 0,sayi=0;
+            //while (true) 
+            //{
+
+            //    Console.WriteLine("Sayi Giriniz");
+            //    sayi=int.Parse(Console.ReadLine());
+            //    if (sayi % 2 == 0) cifttoplam++;
+            //    else tektoplam++;
+
+
+            //    Console.WriteLine("Cikmak icin f9 'a basiniz ..");
+            //    ConsoleKeyInfo key = Console.ReadKey();
+
+            //    if (key.Key == ConsoleKey.F9)
+            //    {
+            //        break; //Donguyu kirmaya yarar.
+            //    }
+
+
+
+            //}
+
+
+            #endregion
+
+
+            #region While ile sayi tahmin oyunu
+
+            //int tahminhakki = 10, tutulansayi = 1, tahmin = 0,sayac=0;
+            //Random rnd = new Random();
+            //tutulansayi = rnd.Next(1, 50);
+            //1. Yontem
+            //while (true)
+            //{
+            //    Console.WriteLine($"{tahminhakki}. Tahmini Giriniz:");
+            //    tahmin = int.Parse(Console.ReadLine());
+            //    if(tahmin < tutulansayi && tahminhakki>0) 
+            //    {
+            //        Console.WriteLine("Buyutun");
+            //        tahminhakki--;
+            //    }
+            //    else if(tahmin > tutulansayi && tahminhakki > 0)
+            //    {
+            //        Console.WriteLine("Kucultun");
+            //        tahminhakki--;
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine($"Tebrikler {tahminhakki} . denemede buldunuz.");
+            //        break;
+
+            //    }
+            //}
+
+            //2. Yontem
+            //while (tahmin!=tutulansayi)
+            //{
+            //    sayac++;
+            //    Console.WriteLine($"Tahmini Giriniz:");
+            //    tahmin = int.Parse(Console.ReadLine());
+            //    if (tahmin < tutulansayi )
+            //    {
+            //        Console.WriteLine("Buyutun");
+            //        tahminhakki--;
+            //    }
+            //    else if (tahmin > tutulansayi )
+            //    {
+            //        Console.WriteLine("Kucultun");
+            //        tahminhakki--;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Tebrikler {sayac} . denemede buldunuz.");
+            //        break;
+
+            //    }
+            //}
+
+
+
+            #endregion
+
+            #region Do-while ile sayi tahmin oyunu
+            //int tahminhakki = 10, tutulansayi = 1, tahmin = 0, sayac = 0;
+            //Random rnd = new Random();
+            //tutulansayi = rnd.Next(1, 50);
+
+            //do
+            //{
+            //    sayac++;
+
+
+
+            //    Console.WriteLine($"{tahminhakki}. Tahmini Giriniz:");
+            //    tahmin = int.Parse(Console.ReadLine());
+            //    if (tahmin < tutulansayi)
+            //    {
+            //        Console.WriteLine("Buyutun");
+            //        tahminhakki--;
+            //    }
+            //    else if (tahmin > tutulansayi)
+            //    {
+            //        Console.WriteLine("Kucultun");
+            //        tahminhakki--;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Tebrikler {sayac} . denemede buldunuz.");
+            //        break;
+
+            //    }
+
+            //} while (tahmin!=tutulansayi);
+
+            #endregion
+
+            #region For ile sayi tahmin oyunu
+
+            //int tahminhakki = 10, tutulansayi = 1, tahmin = 0, sayac = 0;
+            //Random rnd = new Random();
+            //tutulansayi = rnd.Next(1, 50);
+
+            //for (int i = 0; i < tahminhakki; i++)
+            //{
+            //    Console.WriteLine($"{i+1}. Tahmini Giriniz:");
+            //    tahmin = int.Parse(Console.ReadLine());
+            //    if (tahmin < tutulansayi)
+            //    {
+            //        Console.WriteLine("Buyutun");
+
+            //    }
+            //    else if (tahmin > tutulansayi)
+            //    {
+            //        Console.WriteLine("Kucultun");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Tebrikler {i+1} . denemede buldunuz.");
+            //        break;
+
+            //    }
+
+            //}
+
+
+            #endregion
+
+            #region Carpim Tablosu
+            /*
+             1x1 = 1
+             1x2 = 2
+             .....
+             */
+            #endregion
+
+            #region  Fibanocci 
+            // Kullanicidan ust sinir alalim.. 
+            // Girilen sinira kadar fibanocci sayilarini ekrana yazdirialim
+            //1,1,2,3,5,8,13,21,34......
+
+
+            //Ekrandan Ust Limit Okutuyoruz
+            Console.WriteLine("Ust Limiti Giriniz");
+            int ustLimit = int.Parse(Console.ReadLine());
+
+            // diziyi tanimliyoruz
+            int[] fibonacci = new int[ustLimit];
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < ustLimit; i++)
+            {
+
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+
+
+            }
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+
+
+                if (i > 5)
+                {
+                    Console.WriteLine(fibonacci[i] + "\t" + (fibonacci[i] / Convert.ToSingle(fibonacci[i - 1])));
+                }
+                else
+                {
+                    Console.WriteLine(fibonacci[i]);
+                }
+            }
+
+
+            #endregion
+        }
+    }
+}
---
+++ diziler/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Array => Dizi
+           Birden fazla ayni tipten veriyi bir arada tutmamizi saglayan nesnelerdir
+           Cok karsilasilan hata:  Index was outside the bounds of the array 
+           Eger bu hatayi alirsaniz bilinki dizinin boyutunu asacak bir islem yapmaktasiniz.
+
+            */
+
+            // 1.Tanimlama [] operatoru ile tanimlanir
+            // VeriTipi[] diziAdi = new VeriTip[ElemanSayisi]
+            //int[] sayilar = new int[3];
+            //sayilar[0] = 1;
+            //sayilar[1] = 2;
+            //sayilar[2] = 3;
+            //Burasi hata verecektir.
+            //Verdigi hata :Index was outside the bounds of the array 
+            //sayilar[3] = 4;
+            //sayilar[4] = 5;
+
+            //2. Tanimlama . Object Initilaise
+            //string[] sehirler = new string[3] {"Adana","Ankara","Istanbul" };
+
+            // 3. Tanimlama Yontemi 
+            //string[] sehirler2 = { "Adana", "Ankara", "Istanbul" };
+
+            //char[] alfabe = { 'a', 'b', 'c' };
+
+
+            //double[] alfabe2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
+
+            // Dizinin eleman sayisi .Length propert'si icerisindedir
+            //Console.WriteLine("Dizi Boyutunu Giriniz:");
+            //int boyut = Convert.ToInt32(Console.ReadLine());
+            //int[] sayilar = new int[boyut];
+
+            //int index = 0;
+            //Random rand = new Random();
+            #region While Dongusu
+
+
+            ////While Dongusu 
+            //while (index < boyut) 
+            //{
+            //    sayilar[index] = rand.Next(5,500);
+
+            //    index++;
+            //}
+            #endregion
+
+            #region Do-While Dongusu
+            //index = 0;
+            //do
+            //{
+            //    Console.WriteLine(sayilar[index]);
+            //    index++; 
+            //} while (index<boyut); 
+            #endregion
+
+            #region For Dongusu
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    Console.WriteLine(sayilar[i]);
+            //}
+
+            //string isim = "Ali Veli";
+            //for (int i = 0; i < isim.Length; i++)
+            //{
+            //    Console.WriteLine(isim[i]);
+            //} 
+            #endregion
+
+
+            #region Rastgele olusturulan 50 elemanli bir dizinin Tek ve cift sayilarin miktari
+
+            //int[] sayilar2 = new int[50];
+            //int cift=0, tek=0;
+
+            //for (int i = 0; i < sayilar2.Length; i++)
+            //{
+            //    sayilar2[i] = rand.Next(1, 100);
+
+            //}
+
+            //for (int i = 0; i < sayilar2.Length; i++)
+            //{
+            //    if (sayilar2[i] % 2 == 0)
+            //    {
+            //        cift++;
+            //    }
+            //    else
+            //    {
+            //        tek++;
+            //    }
+            //}
+
+            //Console.WriteLine("-------------------- Dizi Dokumu----------------");
+            //for (int i = 0; i < sayilar2.Length; i++)
+            //{
+            //    Console.WriteLine(sayilar2[i]);
+            //}
+            //Console.WriteLine("-------------------------------------------------");
+            //Console.WriteLine("Dizideki Tek eleman sayisi:"+tek);
+            //Console.WriteLine("Dizideki Cift eleman sayisi:" + cift);
+
+
+            #endregion
+
+            #region Ekrandan okutulan sayini faktoryeli ni bulun
+
+            Console.WriteLine("Sayi Giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sonuc = 1;
+            for (int i = sayi; i > 1; i--)
+            {
+                sonuc *= i;
+            }
+            Console.WriteLine($"Girilen {sayi} nin faktoryeli {sonuc} dir");
+            #endregion
+        }
+    }
+}
